929d4917300ff4691e224b7ccc6de6528c83c943
bebeshen <obebesheno@gmail.com>
2024-01-11 13:52:50 +0800

    Init project

---
+++ sort/bubblesort.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    
+    vector<int> v{ 56, 27, 38, 48, 61, 76, 13, 27, 88, 2};
+
+    return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,21 @@
+/*
+    Bubble sort
+
+    Steps:
+        1. 
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    /*
+        input  : 56, 27, 38, 48, 61, 76, 13, 88, 93, 2
+        output : 2, 13, 27, 38, 48, 56, 61, 76, 88, 93
+    */
+    vector<int> v{ 56, 27, 38, 48, 61, 76, 13, 88, 93, 2};
+
+    return 0;
+}
