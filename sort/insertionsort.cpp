97fcf1f202bfcd1e243d4493fc7be057022ea0f8
bebeshen <obebesheno@gmail.com>
2024-01-18 11:01:18 +0800

    Insertion sort

---
+++ sort/insertionsort.cpp
@@ -0,0 +1,43 @@
+/*
+    Insertion sort
+
+    Steps:
+        1. Traverse from left and compare adjacent elements and the higher one is placed at right side. 
+            ( In this way, the largest element is moved to the rightmost end at first. )
+        2. This process is then continued to find the second largest and place it and so on until the data is sorted.
+
+    Time Complexity     : O(N^2)
+    Space Complexity    : O(1)
+
+    ref: https://jimmyswebnote.com/bubble-sort/
+*/
+#include<iostream>
+#include<vector>
+#include"com.h"
+
+using namespace std;
+
+void Insertionsort(vector<int>& v){
+    int n = v.size();
+    for(int i=1;i<n;i++){
+        int j = i;
+        while(v[j]<v[j-1]){
+            swap(v[j], v[j-1]);
+            j--;
+        }
+    }
+}
+
+int main(){
+    /*
+        input  : 56, 27, 38, 48, 61, 76, 13, 88, 93, 2
+        output : 2, 13, 27, 38, 48, 56, 61, 76, 88, 93
+    */
+    int arr[] = { 56, 27, 38, 48, 61, 76, 13, 27, 88, 2};
+    vector<int> v(arr, arr+10);
+    cout<<"Before sorting: ";    print_v(v);
+    Insertionsort(v);
+    cout<<"After sorting: ";    print_v(v);
+
+    return 0;
+}
